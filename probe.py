dd52ee9d67a980dfa36462ccc4690e6030ad6654
RasimKuchchaev <rasimkuchchaev@gmail.com>
2025-04-09 11:40:00 +0300

    2.2 Обработка подключения, отключения и событий. Практика 2

--- probe.py
+++ probe.py
@@ -2,6 +2,7 @@ import eventlet
 import socketio
 
 users_count = []
+lost_queries = []
 # Создаем экземпляр синхронного сервера Socket.IO
 sio = socketio.Server()
 
@@ -20,18 +21,25 @@ def disconnect(sid):
     users_count.remove(sid)
     print(f"Клиент {sid} отключен")
 
-
 # Обработка событий message
 @sio.on("message")
 def incoming_message(sid, data):
     print("мне сказали", data, sid)
-
 
 @sio.on('get_users_online')
 def get_user_all(sid, data):
     online_users = len(users_count)
     print(f"online users: {online_users}  {sid}")
     sio.emit("users", {"online": online_users})
+
+@sio.on("*")
+def add_query(event, sid, data):
+    lost_queries.append(sid)
+
+@sio.on("count_queries")
+def get_count_queries(sid, data):
+    count_query = len(lost_queries)
+    sio.emit(event="queries", data={"lost": count_query})
 
 
 # Запускаем eventlet веб-сервер
