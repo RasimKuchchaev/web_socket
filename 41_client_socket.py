d43585bd1d8a93f99c2c4f4ec870a87c0b8a314e
RasimKuchchaev <rasimkuchchaev@gmail.com>
2025-04-16 11:36:22 +0300

    4.1 Клиента веб-сокета добавляем обработчик событий

--- 41_client_socket.py
+++ 41_client_socket.py
@@ -5,3 +5,7 @@ sio = socketio.SimpleClient()
 
 # Подключение к серверу
 sio.connect('ws://0.0.0.0:80')
+
+# получение события от сервера используется sio.receive()
+event, data = sio.receive()
+print(event, data)
